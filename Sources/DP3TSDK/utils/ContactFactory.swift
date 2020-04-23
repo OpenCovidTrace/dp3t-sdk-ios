b34abb0de24b69e1322920777ed1ae84d10592d1
Stefan Mitterrutzner <stefan.mitterrutzner@gmail.com>
2020-04-23 16:31:13 +0200

    beginning of sliding window implementation

--- Sources/DP3TSDK/utils/ContactFactory.swift
+++ Sources/DP3TSDK/utils/ContactFactory.swift
@@ -7,6 +7,7 @@
 import Foundation
 
 enum ContactFactory {
+    static let badRssiThreshold: Double = -85.0
     /// Helper function to create contacts from handshakes
     /// - Parameters:
     ///   - contactThreshold: how many handshakes to have to be recognized as contact
@@ -25,7 +26,35 @@ enum ContactFactory {
 
         let contacts: [Contact] = groupedHandshakes.compactMap { element -> Contact? in
             //filter result to only contain ephIDs which have been seen more than contactThreshold times
-            guard element.value.count > contactThreshold else { return nil }
+            let ephID = element.key
+            let handshakes = element.value
+
+            let rssiValues: [(Date, Double)] = handshakes.compactMap { handshake -> (Date, Double)? in
+                guard let rssi = handshake.RSSI else { return nil }
+                guard rssi > ContactFactory.badRssiThreshold else { return nil }
+                return (handshake.timestamp, rssi)
+            }
+
+            guard let firstValue = rssiValues.first else { return nil }
+
+            let meanEpochRssi = rssiValues.map{ $0.1 }.reduce(0.0, +) / Double(rssiValues.count)
+
+            let epochStart = DP3TCryptoModule.getEpochStart(timestamp: firstValue.0)
+
+            let windowMeans: [Double] = (0 ..< Int(CryptoConstants.secondsPerEpoch / TimeInterval.minute)).compactMap { (index) -> Double? in
+                let start = epochStart.addingTimeInterval(TimeInterval(index) * .second)
+                let end = start.addingTimeInterval(.minute)
+                let values = rssiValues.filter { (timestamp, rssi) -> Bool in
+                    return timestamp > start && timestamp < end
+                }.map{ $0.1 }
+                if values.isEmpty {
+                    return nil
+                } else {
+                    return values.reduce(0.0, +) / Double(values.count)
+                }
+            }
+
+
             let day = DayDate(date: element.value.first!.timestamp)
             return Contact(identifier: nil, ephID: element.key, day: day, associatedKnownCase: nil)
         }
